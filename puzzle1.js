580dbab6af131477676f31bea8d9ea1d3cb80372
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 13:38:53 -0800

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,21 @@
+//give instructions to Rudy, the red dot, so that it passes over (collects) all the green dots.
+//puzzle 1
+//Rudy, the red dot, can perform: up, down, left, right, getColor, and setColor.
+
+var steps = 0
+while (steps < 8) {
+  down
+  steps = step + 1
+}
+var rightMoves = 0
+while (rightMoves < 5) {
+  right
+  rightMoves = rightMoves + 1;
+}
+up
+up
+//collect first green dot
+right
+right
+//collect second green dot
+//program finished
