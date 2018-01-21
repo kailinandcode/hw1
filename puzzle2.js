82f312798b2f171827d827bec7dd6fb7f129271c
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 13:43:16 -0800

    Create puzzle2.js

---
+++ puzzle2.js
@@ -0,0 +1,18 @@
+var steps = 0
+while (steps < 6) {
+  down
+  steps = steps + 1
+ }
+ right 
+ right
+ right
+ up
+ up
+ right
+ right
+ down
+ //collect first green
+ down
+ down
+ //collect second green
+ //program ends
