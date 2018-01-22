5bad0d43f86807dc813c4f5a1e985c05caa2e886
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 21:32:58 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,22 @@
+//puzzle 4
+function moveDown5() {
+  var x = 0;
+  while (x < 5) {
+  down();
+  x += 1;
+  }
+}
+function collectGreenDots() {
+  var dotsLeft = 2;
+  while (dotsLeft != 0) {
+    right();
+    up();
+    var FirstGate = getColor();
+    down();
+    setColor = FirstGate();
+    //passes gate and collects green dot
+    right();
+    right();
+    dotsLeft -= 1;
+  }
+}
