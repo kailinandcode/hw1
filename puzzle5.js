8ee3b70137521a42badfca4207d6fdfff219831e
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 21:40:30 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,5 @@
+//puzzle 5
+//to avoid obstacles
+//when encountering a red square, Rudy should move up before continuing right
+//when encountering a blue square, Rudy should move down before continuing right
+
