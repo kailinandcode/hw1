8104ad937452724410afefcc2e921a650881e648
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 21:52:26 -0800

    Update puzzle5.js

--- puzzle5.js
+++ puzzle5.js
@@ -3,3 +3,18 @@
 //when encountering a red square, Rudy should move up before continuing right
 //when encountering a blue square, Rudy should move down before continuing right
 
+function navigate() {
+  while (remainingDots() != 0) {
+    if (getColor() != "blue" | getColor() != "red") {
+      right();
+    }
+    else if (getColor() == "blue") {
+      down();
+    }
+    else {
+      up();
+    }
+  }
+}
+
+navigate();
