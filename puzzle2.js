38c5aee87bfe356aa20d51a85f3228e2bab39204
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-21 21:43:07 -0800

    Update puzzle2.js

--- puzzle2.js
+++ puzzle2.js
@@ -1,18 +1,20 @@
-var steps = 0
+//puzzle 2
+
+var steps = 0;
 while (steps < 6) {
-  down（）
-  steps = steps + 1
+  down();
+  steps = steps + 1;
  }
- right（）
- right（）
- right（）
- up（）
- up（）
- right（）
- right（）
- down（）
+ right();
+ right();
+ right();
+ up();
+ up();
+ right();
+ right();
+ down();
  //collect first green
- down（）
- down（）
+ down();
+ down();
  //collect second green
  //program ends
