8cf3d3d4774e54e726cbfd1b701fc85d2a49723b
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-22 21:44:25 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,6 @@
+//puzzle 6
+// if the first thing you encountered was red, move up 3 more
+// otherise, move down 3 more
+// if the second colored square is a blue, move right-up
+// if the second colored square is a red, move left-up
+
