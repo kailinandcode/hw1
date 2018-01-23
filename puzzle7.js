1c1ee6dbe6c2bb5f314f0dc05049b751b90a748c
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-23 11:56:10 -0800

    Update puzzle7.js

--- puzzle7.js
+++ puzzle7.js
@@ -1,2 +1,29 @@
 //down, up, down up
 //everytime you encounter red, go right twice then move vertically OPP direction
+
+function skipEveryOther() {
+  while (getColor != "red") {
+    down();
+  }
+  right();
+  right();
+  while (getColor != "red") {
+    up();
+  }
+  right();
+  right();
+  while (getColor != "red") {
+    down();
+  }
+  right();
+  right();
+  while (getColor != "red") {
+    up();
+  }
+  right();
+  right();
+  while (getColor != "red") {
+    down();
+  }
+  right();
+}
