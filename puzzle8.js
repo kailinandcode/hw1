8b9722f85c6930e4585a6fa5803e31ab54d4385e
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-23 16:21:34 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,1 +1,30 @@
 
+function traverseDown8() {
+  var step = 0;
+  while (step < 8) {
+    down();
+  }
+  right();
+  right();
+}
+traverseDown8();
+function traverse7Up() {
+  var step = 0;
+  while (step < 7) {
+    up();
+  }
+  right();
+  right();
+}
+traverse7Up();
+function traverse7Down() {
+  var step = 0;
+  while (step < 7) {
+    down();
+  }
+  right();
+}
+traverse7Down();
+right();
+traverse7Up();
+traverse7Down();
